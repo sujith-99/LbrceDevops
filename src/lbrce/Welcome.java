4d7e7e44dd7395aa9da873eb2b2775af16060a42
AdmiN <AdmiN@ITWS063>
2024-08-21 14:57:52 +0530

    Committed

---
+++ src/lbrce/Welcome.java
@@ -0,0 +1,11 @@
+package lbrce;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Devops Lab.");
+
+	}
+
+}
